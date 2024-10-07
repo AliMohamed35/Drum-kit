f71a1636df71e9457620a449310710630eb1406b
AliMohamed <137190242+AliMohamed35@users.noreply.github.com>
2024-10-06 20:12:25 -0700

    added new lines of code

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
+window.addEventListener('keydown', playSound)
+
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return; // this prevents the function from running.
-    audio.currentTime = 0; // rewind the audio to the start
+    if (!audio) return; // this is to prevent function from working
+    audio.currentTime = 0; // this is to re-wind the audio
     audio.play();
-    key.classList.add("playing");
+    key.classList.add('playing');
 }
+
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return; // skip it if it's not transform
-    console.log(this);
+    if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll(".key");
+const keys = document.querySelectorAll('.key'); // this returns NodeList soI can use forEach().
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener("keydown", playSound)
+
