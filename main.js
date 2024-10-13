6654dd1b641e272ebbb582ae61a1cf405c233d9d
AliMohamed <137190242+AliMohamed35@users.noreply.github.com>
2024-10-13 15:39:43 -0700

    added more comments

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
-window.addEventListener('keydown', playSound)
+// add event listner to the window.
+window.addEventListener('keydown', playSound);
 
+// Created a function to play sound when event is done
 function playSound(e) {
+    const keys = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return; // this is to prevent function from working
-    audio.currentTime = 0; // this is to re-wind the audio
+
+    if (!audio) return;
+    audio.currentTime = 0; // this will rewind the sound
     audio.play();
-    key.classList.add('playing');
+    keys.classList.add('playing');
 }
 
+// Create function to remove the transition.
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
 
-const keys = document.querySelectorAll('.key'); // this returns NodeList soI can use forEach().
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+// add event listeners to seek for any changes on the buttons:
 
+// this returns NodeList so I can use forEach():
+const keys = document.querySelectorAll('.key');
+// this returns NodeList so I can use forEach():
+
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); 
